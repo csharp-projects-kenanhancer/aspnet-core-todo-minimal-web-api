62d884e5d9c6f8b2268a4be50ebf1d7856c83655
kenan <kenan@10dot1.co.uk>
2023-07-13 10:55:28 +0100

    db seeding and some updates in endpoints are done

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 
@@ -36,6 +37,7 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 var app = builder.Build();
 
+
 // Configure the HTTP request pipeline:
 if (app.Environment.IsDevelopment())
 {
@@ -49,113 +51,146 @@ if (app.Environment.IsDevelopment())
         options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
         options.RoutePrefix = string.Empty;
     });
+
+    // Seed database
+    using (var scope = app.Services.CreateScope())
+    {
+        var scopedServices = scope.ServiceProvider;
+        var database = scopedServices.GetRequiredService<TodoDb>();
+
+        // Ensure the database is created (only applicable for non InMemory providers)
+        // database.Database.EnsureCreated();
+
+        List<Todo> todos = new List<Todo>()
+        {
+            new Todo { Id = Guid.Parse("11111111-1111-1111-1111-111111111111"), Name = "Buy groceries", IsComplete = false },
+            new Todo { Id = Guid.Parse("22222222-2222-2222-2222-222222222222"), Name = "Finish project report", IsComplete = true },
+            new Todo { Id = Guid.Parse("33333333-3333-3333-3333-333333333333"), Name = "Book doctor appointment", IsComplete = false },
+            new Todo { Id = Guid.Parse("44444444-4444-4444-4444-444444444444"), Name = "Walk the dog", IsComplete = true },
+            new Todo { Id = Guid.Parse("55555555-5555-5555-5555-555555555555"), Name = "Prepare for meeting", IsComplete = false },
+            new Todo { Id = Guid.Parse("66666666-6666-6666-6666-666666666666"), Name = "Pick up kids from school", IsComplete = true },
+            new Todo { Id = Guid.Parse("77777777-7777-7777-7777-777777777777"), Name = "Call the bank", IsComplete = false },
+            new Todo { Id = Guid.Parse("88888888-8888-8888-8888-888888888888"), Name = "Study for the exam", IsComplete = true },
+            new Todo { Id = Guid.Parse("99999999-9999-9999-9999-999999999999"), Name = "Clean the house", IsComplete = false },
+            new Todo { Id = Guid.Parse("00000000-0000-0000-0000-000000000000"), Name = "Pay the bills", IsComplete = true }
+        };
+
+        database.Todos.AddRange(todos);
+
+        database.SaveChanges();
+    }
 }
 
 // Endpoints:
 
-app.MapGet("/todoitems", async (TodoDb db) =>
-{
-    return await db.Todos.ToListAsync();
-})
-.WithName("ListTodos")
-.WithDescription("Fetches the todo list")
-.WithSummary("Fetches the todo list")
-.WithTags("Get Operations")
-.WithOpenApi();
+app.MapGet("/todos", async (TodoDb db) => { return await db.Todos.ToListAsync(); })
+    .WithName("ListTodos")
+    .WithDescription("Fetches the todo list")
+    .WithSummary("Fetches the todo list")
+    .WithTags("Get Operations")
+    .WithOpenApi();
 
-app.MapGet("/todoitems/complete", async (TodoDb db) =>
-{
-    return await db.Todos.Where(t => t.IsComplete).ToListAsync();
-})
-.WithName("ListCompletedTodos")
-.WithDescription("Fetches the completed todo list")
-.WithSummary("Fetches the completed todo list")
-.WithTags("Get Operations")
-.WithOpenApi();
+app.MapGet("/todos/completed", async (TodoDb db) => { return await db.Todos.Where(t => t.IsComplete).ToListAsync(); })
+    .WithName("ListCompletedTodos")
+    .WithDescription("Fetches the completed todo list")
+    .WithSummary("Fetches the completed todo list")
+    .WithTags("Get Operations")
+    .WithOpenApi();
 
-app.MapGet("/todoitems/{id}", async (int id, TodoDb db) =>
-{
-    return await db.Todos.FindAsync(id) is Todo todo ? Results.Ok(todo) : Results.NotFound();
-})
-.WithOpenApi(operation =>
-    new OpenApiOperation(operation)
-    {
-        OperationId = "GetTodoById",
-        Description = "Get todo by id",
-        Summary = "Get todo by id",
-        Tags = new List<OpenApiTag>() { new OpenApiTag() { Name = "Get Operations" } },
-        Parameters = new List<OpenApiParameter>()
+app.MapGet("/todos/incomplete", async (TodoDb db) => { return await db.Todos.Where(t => !t.IsComplete).ToListAsync(); })
+    .WithName("ListIncompleteTodos")
+    .WithDescription("Fetches the todo list")
+    .WithSummary("Fetches the todo list")
+    .WithTags("Get Operations")
+    .WithOpenApi();
+
+// endpoint open API definition can be done in WithOpenApi function as well.
+app.MapGet("/todos/{id:guid}",
+        async ([FromRoute] Guid id, TodoDb db) =>
         {
-            new OpenApiParameter(operation.Parameters[0])
+            return await db.Todos.FindAsync(id) is Todo todo ? Results.Ok(todo) : Results.NotFound();
+        })
+    .WithOpenApi(operation =>
+        new OpenApiOperation(operation)
+        {
+            OperationId = "GetTodoById",
+            Description = "Get todo by id",
+            Summary = "Get todo by id",
+            Tags = new List<OpenApiTag>() { new OpenApiTag() { Name = "Get Operations" } },
+            Parameters = new List<OpenApiParameter>()
             {
-                Description = "The ID associated with the created Todo"
+                new OpenApiParameter(operation.Parameters[0])
+                {
+                    Description = "The ID associated with the created Todo"
+                }
             }
         }
-    }
-);
+    );
 
-app.MapPost("/todoitems", async (Todo todo, TodoDb db) =>
-{
-    db.Todos.Add(todo);
-    await db.SaveChangesAsync();
-
-    return Results.Created($"/todoitems/{todo.Id}", todo);
-})
-.WithName("AddTodoItem")
-.WithDescription("Add todo item")
-.WithSummary("Add todo item")
-.WithTags("Set Operations")
-.WithOpenApi();
-
-app.MapPut("/todoitems/{id}", async (int id, Todo inputTodo, TodoDb db) =>
-{
-    var todo = await db.Todos.FindAsync(id);
-
-    if (todo is null) return Results.NotFound();
-
-    todo.Name = inputTodo.Name;
-    todo.IsComplete = inputTodo.IsComplete;
-
-    await db.SaveChangesAsync();
-
-    return Results.NoContent();
-})
-.WithName("ReplaceTodoItem")
-.WithDescription("Replace Todo Item")
-.WithSummary("Replace Todo Item")
-.WithTags("Set Operations")
-.WithOpenApi();
-
-app.MapDelete("/todoitems/{id}", async (int id, TodoDb db) =>
-{
-    if (await db.Todos.FindAsync(id) is Todo todo)
+app.MapPost("/todos", async ([FromBody] Todo todo, TodoDb db) =>
     {
-        db.Todos.Remove(todo);
+        db.Todos.Add(todo);
         await db.SaveChangesAsync();
 
-        return Results.Ok(todo);
-    }
+        return Results.Created($"/todos/{todo.Id}", todo);
+    })
+    .WithName("AddTodoItem")
+    .WithDescription("Add todo item")
+    .WithSummary("Add todo item")
+    .WithTags("Set Operations")
+    .WithOpenApi();
 
-    return Results.NotFound();
-})
-.WithName("DeleteTodoItemById")
-.WithDescription("Delete Todo Item by Id")
-.WithSummary("Delete Todo Item by Id")
-.WithTags("Set Operations")
-.WithOpenApi();
+app.MapPut("/todos/{id:guid}", async ([FromRoute] Guid id, [FromBody] Todo inputTodo, TodoDb db) =>
+    {
+        var todo = await db.Todos.FindAsync(id);
+
+        if (todo is null) return Results.NotFound();
+
+        todo.Name = inputTodo.Name;
+        todo.IsComplete = inputTodo.IsComplete;
+
+        await db.SaveChangesAsync();
+
+        return Results.NoContent();
+    })
+    .WithName("ReplaceTodoItem")
+    .WithDescription("Replace Todo Item")
+    .WithSummary("Replace Todo Item")
+    .WithTags("Set Operations")
+    .WithOpenApi();
+
+app.MapDelete("/todos/{id:guid}", async ([FromRoute] Guid id, TodoDb db) =>
+    {
+        if (await db.Todos.FindAsync(id) is Todo todo)
+        {
+            db.Todos.Remove(todo);
+            await db.SaveChangesAsync();
+
+            return Results.Ok(todo);
+        }
+
+        return Results.NotFound();
+    })
+    .WithName("DeleteTodoItemById")
+    .WithDescription("Delete Todo Item by Id")
+    .WithSummary("Delete Todo Item by Id")
+    .WithTags("Set Operations")
+    .WithOpenApi();
 
 app.Run();
 
 class Todo
 {
-    public int Id { get; set; }
+    public Guid Id { get; set; }
     public string? Name { get; set; }
     public bool IsComplete { get; set; }
 }
 
 class TodoDb : DbContext
 {
-    public TodoDb(DbContextOptions<TodoDb> options) : base(options) { }
+    public TodoDb(DbContextOptions<TodoDb> options) : base(options)
+    {
+    }
 
     public DbSet<Todo> Todos => Set<Todo>();
 }
--- README.md
+++ README.md
@@ -1,41 +1,61 @@
 # aspnet-core-todo-minimal-web-api
+
 ASP.NET Core Todo Minimal Web API
 
 ![](/assets/image1.png)
 
 ## Build Project
+
 ```shell
 dotnet build
 ```
 
 ## Run Project
+
 ```shell
 dotnet run
 ```
 
 > Update port number in curl requests after running project. You can find it in Debug Console
 
-## All Todos
+## Fetches all todos
+
 ```shell
-curl https://localhost:7225/todoitems
+curl https://localhost:7235/todos
 ```
 
-## Completed Todos
+## Fetches completed todos
+
 ```shell
-curl https://localhost:7225/todoitems/complete
+curl https://localhost:7235/todos/completed
+```
+
+## Fetches incomplete todos
+
+```shell
+curl https://localhost:7235/todos/incomplete
+```
+
+## Get todo by id
+
+```shell
+curl https://localhost:7235/todos/33333333-3333-3333-3333-333333333333
 ```
 
 ## Create Todo
+
 ```shell
-curl -X POST -H "Content-Type: application/json" -d '{"name":"walk dog","isComplete":true}' https://localhost:7225/todoitems
+curl -X POST -H "Content-Type: application/json" -d '{"name":"walk dog","isComplete":true}' https://localhost:7235/todos
 ```
 
 ## Update Todo
+
 ```shell
-curl -X PUT -H "Content-Type: application/json" -d '{"name":"walk dog", "isComplete":false}' https://localhost:7225/todoitems/1
+curl -X PUT -H "Content-Type: application/json" -d '{"name":"walk dog", "isComplete":false}' https://localhost:7235/todos/33333333-3333-3333-3333-333333333333
 ```
 
 ## Swagger UI Endpoints
+
 ```
 http://localhost:5164/swagger
 ```
