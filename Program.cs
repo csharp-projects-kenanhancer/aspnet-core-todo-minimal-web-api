95779eb54211bb8623626c6cf9605408772db619
kenan <kenan@10dot1.co.uk>
2023-07-14 22:39:49 +0100

    endpoint are updated.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
+using FluentValidation;
+using Microsoft.OpenApi.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,12 +13,29 @@ builder.Services.AddEndpointsApiExplorer();
 // Register the Swagger generator, defining 1 or more Swagger documents
 builder.Services.AddSwaggerGen(options =>
 {
-    options.SwaggerDoc("v1", new OpenApiInfo
+    options.SwaggerDoc("user", new OpenApiInfo
     {
-        Version = "v1",
+        Version = "1.0",
+        Title = "User API",
+        Description = "API endpoints for managing users",
+        Contact = new OpenApiContact
+        {
+            Name = "Kenan Hancer",
+            Email = "kenanhancer@gmail.com",
+            Url = new Uri("https://kenanhancer.com")
+        },
+        License = new OpenApiLicense
+        {
+            Name = "User License",
+            Url = new Uri("https://example.com/license")
+        }
+    });
+
+    options.SwaggerDoc("todo", new OpenApiInfo
+    {
+        Version = "1.0",
         Title = "Todo API",
-        Description = "An ASP.NET Core Web API for managing todos",
-        TermsOfService = new Uri("https://example.com/terms"),
+        Description = "API endpoints for managing todos",
         Contact = new OpenApiContact
         {
             Name = "Kenan Hancer",
@@ -33,7 +51,11 @@ builder.Services.AddSwaggerGen(options =>
 });
 
 builder.Services.AddDbContext<TodoDb>(opt => opt.UseInMemoryDatabase("TodoList"));
+builder.Services.AddSingleton<IDatabaseInitializer, TodoDbInitializer>();
+builder.Services.AddSingleton<IDateTimeProvider, DateTimeProvider>();
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+builder.Services.AddValidatorsFromAssemblyContaining<Program>();
+
 
 var app = builder.Build();
 
@@ -48,75 +70,53 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI(options =>
     {
         // To serve the Swagger UI at the app's root (https://localhost:<port>/), set the RoutePrefix property to an empty string:
-        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        options.SwaggerEndpoint("/swagger/todo/swagger.json", "Todo API");
+        options.SwaggerEndpoint("/swagger/user/swagger.json", "User API");
         options.RoutePrefix = string.Empty;
     });
 
-    // Seed database
-    using (var scope = app.Services.CreateScope())
-    {
-        var scopedServices = scope.ServiceProvider;
-        var database = scopedServices.GetRequiredService<TodoDb>();
+    IDatabaseInitializer? dbInitializer = app.Services.GetService<IDatabaseInitializer>();
 
-        // Ensure the database is created (only applicable for non InMemory providers)
-        // database.Database.EnsureCreated();
-
-        List<Todo> todos = new List<Todo>()
-        {
-            new Todo { Id = Guid.Parse("11111111-1111-1111-1111-111111111111"), Name = "Buy groceries", IsComplete = false },
-            new Todo { Id = Guid.Parse("22222222-2222-2222-2222-222222222222"), Name = "Finish project report", IsComplete = true },
-            new Todo { Id = Guid.Parse("33333333-3333-3333-3333-333333333333"), Name = "Book doctor appointment", IsComplete = false },
-            new Todo { Id = Guid.Parse("44444444-4444-4444-4444-444444444444"), Name = "Walk the dog", IsComplete = true },
-            new Todo { Id = Guid.Parse("55555555-5555-5555-5555-555555555555"), Name = "Prepare for meeting", IsComplete = false },
-            new Todo { Id = Guid.Parse("66666666-6666-6666-6666-666666666666"), Name = "Pick up kids from school", IsComplete = true },
-            new Todo { Id = Guid.Parse("77777777-7777-7777-7777-777777777777"), Name = "Call the bank", IsComplete = false },
-            new Todo { Id = Guid.Parse("88888888-8888-8888-8888-888888888888"), Name = "Study for the exam", IsComplete = true },
-            new Todo { Id = Guid.Parse("99999999-9999-9999-9999-999999999999"), Name = "Clean the house", IsComplete = false },
-            new Todo { Id = Guid.Parse("00000000-0000-0000-0000-000000000000"), Name = "Pay the bills", IsComplete = true }
-        };
-
-        database.Todos.AddRange(todos);
-
-        database.SaveChanges();
-    }
+    dbInitializer?.Initialize();
 }
 
 // Endpoints:
 
-app.MapGet("/todos", async (TodoDb db) => { return await db.Todos.ToListAsync(); })
-    .WithName("ListTodos")
-    .WithDescription("Fetches the todo list")
-    .WithSummary("Fetches the todo list")
-    .WithTags("Get Operations")
+RouteGroupBuilder todosRoute = app.MapGroup("/todos");
+
+todosRoute.MapGet("/", GetAllTodos)
+    .WithName("GetAllTodos")
+    .WithDescription("Returns a list of all todo items.")
+    .WithSummary("Retrieve all todo items")
+    .WithTags("Todo Read Operations")
+    .WithGroupName("todo")
     .WithOpenApi();
 
-app.MapGet("/todos/completed", async (TodoDb db) => { return await db.Todos.Where(t => t.IsComplete).ToListAsync(); })
-    .WithName("ListCompletedTodos")
-    .WithDescription("Fetches the completed todo list")
-    .WithSummary("Fetches the completed todo list")
-    .WithTags("Get Operations")
+todosRoute.MapGet("/completed", GetCompletedTodos)
+    .WithName("GetCompletedTodos")
+    .WithDescription("Returns a list of completed todo items.")
+    .WithSummary("Retrieve completed todo items")
+    .WithTags(tags: "Todo Read Operations")
+    .WithGroupName("todo")
     .WithOpenApi();
 
-app.MapGet("/todos/incomplete", async (TodoDb db) => { return await db.Todos.Where(t => !t.IsComplete).ToListAsync(); })
-    .WithName("ListIncompleteTodos")
-    .WithDescription("Fetches the todo list")
-    .WithSummary("Fetches the todo list")
-    .WithTags("Get Operations")
+todosRoute.MapGet("/active", GetActiveTodos)
+    .WithName("GetActiveTodos")
+    .WithDescription("Returns a list of active todo items.")
+    .WithSummary("Retrieve active todo items")
+    .WithTags("Todo Read Operations")
+    .WithGroupName("todo")
     .WithOpenApi();
 
-// endpoint open API definition can be done in WithOpenApi function as well.
-app.MapGet("/todos/{id:guid}",
-        async ([FromRoute] Guid id, TodoDb db) =>
-        {
-            return await db.Todos.FindAsync(id) is Todo todo ? Results.Ok(todo) : Results.NotFound();
-        })
+todosRoute.MapGet("/{id:guid}", GetTodo)
+    .WithGroupName("todo")
     .WithOpenApi(operation =>
         new OpenApiOperation(operation)
         {
             OperationId = "GetTodoById",
-            Description = "Get todo by id",
-            Summary = "Get todo by id",
-            Tags = new List<OpenApiTag>() { new OpenApiTag() { Name = "Get Operations" } },
+            Description = "Returns todo item with the specified ID.",
+            Summary = "Retrieve a specific todo item",
+            Tags = new List<OpenApiTag>() { new OpenApiTag() { Name = "Todo Read Operations" } },
             Parameters = new List<OpenApiParameter>()
             {
                 new OpenApiParameter(operation.Parameters[0])
@@ -127,64 +127,186 @@ app.MapGet("/todos/{id:guid}",
         }
     );
 
-app.MapPost("/todos", async ([FromBody] Todo todo, TodoDb db) =>
-    {
-        db.Todos.Add(todo);
-        await db.SaveChangesAsync();
-
-        return Results.Created($"/todos/{todo.Id}", todo);
-    })
+todosRoute.MapPost("/", CreateTodo)
     .WithName("AddTodoItem")
     .WithDescription("Add todo item")
     .WithSummary("Add todo item")
-    .WithTags("Set Operations")
+    .WithTags("Todo Write Operations")
+    .WithGroupName("todo")
     .WithOpenApi();
 
-app.MapPut("/todos/{id:guid}", async ([FromRoute] Guid id, [FromBody] Todo inputTodo, TodoDb db) =>
-    {
-        var todo = await db.Todos.FindAsync(id);
-
-        if (todo is null) return Results.NotFound();
-
-        todo.Name = inputTodo.Name;
-        todo.IsComplete = inputTodo.IsComplete;
-
-        await db.SaveChangesAsync();
-
-        return Results.NoContent();
-    })
+todosRoute.MapPut("/{id:guid}", ReplaceTodo)
     .WithName("ReplaceTodoItem")
-    .WithDescription("Replace Todo Item")
+    .WithDescription("Updates todo item with the specified ID.")
     .WithSummary("Replace Todo Item")
-    .WithTags("Set Operations")
+    .WithTags("Todo Write Operations")
+    .WithGroupName("todo")
     .WithOpenApi();
 
-app.MapDelete("/todos/{id:guid}", async ([FromRoute] Guid id, TodoDb db) =>
-    {
-        if (await db.Todos.FindAsync(id) is Todo todo)
-        {
-            db.Todos.Remove(todo);
-            await db.SaveChangesAsync();
+todosRoute.MapPatch("/{id:guid}/status", UpdateTodoStatus)
+    .WithName("UpdateTodoStatus")
+    .WithDescription("Updates todo status with the specified ID.")
+    .WithSummary("Update Todo Status")
+    .WithTags("Todo Write Operations")
+    .WithGroupName("todo")
+    .WithOpenApi();
 
-            return Results.Ok(todo);
-        }
+todosRoute.MapPatch("/{id:guid}/complete", MarkTodoAsCompleted)
+    .WithName("MarkTodoAsComplete")
+    .WithDescription("Marks a specific todo item as complete.")
+    .WithSummary("Mark a specific todo item as complete")
+    .WithTags("Todo Write Operations")
+    .WithGroupName("todo")
+    .WithOpenApi();
 
-        return Results.NotFound();
-    })
-    .WithName("DeleteTodoItemById")
-    .WithDescription("Delete Todo Item by Id")
-    .WithSummary("Delete Todo Item by Id")
-    .WithTags("Set Operations")
+todosRoute.MapPatch("/{id}/active", MarkTodoAsActive)
+    .WithName("MarkTodoAsActive")
+    .WithDescription("Marks a specific todo item as active (not complete).")
+    .WithSummary("Mark a specific todo item as active")
+    .WithTags("Todo Write Operations")
+    .WithGroupName("todo")
+    .WithOpenApi();
+
+todosRoute.MapDelete("/{id:guid}", DeleteTodo)
+    .WithName("DeleteTodo")
+    .WithDescription("Deletes todo item with the specified ID.")
+    .WithSummary("Delete a specific todo item")
+    .WithTags("Todo Write Operations")
+    .WithGroupName("todo")
     .WithOpenApi();
 
 app.Run();
+
+static async Task<IResult> GetAllTodos(TodoDb db)
+{
+    return TypedResults.Ok(await db.Todos.ToArrayAsync());
+}
+
+static async Task<IResult> GetCompletedTodos(TodoDb db)
+{
+    return TypedResults.Ok(await db.Todos.Where(t => t.IsCompleted).ToListAsync());
+}
+
+static async Task<IResult> GetActiveTodos(TodoDb db)
+{
+    return TypedResults.Ok(await db.Todos.Where(t => !t.IsCompleted).ToListAsync());
+}
+
+static async Task<IResult> GetTodo([FromRoute] Guid id, TodoDb db)
+{
+    return await db.Todos.FindAsync(id)
+        is Todo todo
+            ? TypedResults.Ok(todo)
+            : TypedResults.NotFound();
+}
+
+static async Task<IResult> CreateTodo([FromBody] Todo todo, TodoDb db)
+{
+    db.Todos.Add(todo);
+    await db.SaveChangesAsync();
+
+    return TypedResults.Created($"/todos/{todo.Id}", todo);
+}
+
+static async Task<IResult> ReplaceTodo([FromRoute] Guid id, [FromBody] Todo inputTodo, TodoDb db)
+{
+    Todo? todo = await db.Todos.FindAsync(id);
+
+    if (todo is null) return Results.NotFound();
+
+    todo.Name = inputTodo.Name;
+    todo.IsCompleted = inputTodo.IsCompleted;
+
+    await db.SaveChangesAsync();
+
+    return TypedResults.NoContent();
+}
+
+static async Task<IResult> UpdateTodoStatus([FromRoute] Guid id, [FromBody] TodoStatusUpdate todoStatusUpdate, TodoDb db)
+{
+    if (todoStatusUpdate == null)
+    {
+        return TypedResults.BadRequest();
+    }
+
+    Todo? todo = await db.Todos.FindAsync(id);
+
+    if (todo == null)
+    {
+        return TypedResults.NotFound();
+    }
+
+    todo.IsCompleted = todoStatusUpdate.IsCompleted;
+    await db.SaveChangesAsync();
+
+    return TypedResults.NoContent();
+}
+
+static async Task<IResult> MarkTodoAsCompleted([FromRoute] Guid id, TodoDb db)
+{
+    var todo = await db.Todos.FindAsync(id);
+
+    if (todo == null)
+    {
+        return TypedResults.NotFound();
+    }
+
+    todo.IsCompleted = true;
+    await db.SaveChangesAsync();
+
+    return TypedResults.NoContent();
+}
+
+static async Task<IResult> MarkTodoAsActive([FromRoute]Guid id, TodoDb db)
+{
+    var todo = await db.Todos.FindAsync(id);
+    if (todo == null)
+    {
+        return Results.NotFound();
+    }
+
+    todo.IsCompleted = false;
+    await db.SaveChangesAsync();
+
+    return Results.NoContent();
+}
+
+static async Task<IResult> DeleteTodo([FromRoute] Guid id, TodoDb db)
+{
+    if (await db.Todos.FindAsync(id) is Todo todo)
+    {
+        db.Todos.Remove(todo);
+        await db.SaveChangesAsync();
+
+        return TypedResults.Ok(todo);
+    }
+
+    return TypedResults.NotFound();
+}
 
 class Todo
 {
     public Guid Id { get; set; }
     public string? Name { get; set; }
-    public bool IsComplete { get; set; }
+    public bool IsCompleted { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+    public Guid UserId { get; set; }
+    public User? User { get; set; }
 }
+
+class User
+{
+    public Guid Id { get; set; }
+    public string? FullName { get; set; }
+    public string? Email { get; set; }
+}
+
+record TodoCreateRequest(string Name);
+
+record TodoResponse(Guid Id, string Name, bool IsCompleted, string UserFullName);
+
+record TodoStatusUpdate(bool IsCompleted);
 
 class TodoDb : DbContext
 {
@@ -193,4 +315,97 @@ class TodoDb : DbContext
     }
 
     public DbSet<Todo> Todos => Set<Todo>();
+    public DbSet<User> Users => Set<User>();
+}
+
+class TodoValidator : AbstractValidator<Todo>
+{
+    public TodoValidator()
+    {
+        RuleFor(x => x.Name).NotEmpty().WithMessage("Name must not be empty.");
+        RuleFor(x => x.IsCompleted).Equal(false).WithMessage("IsCompleted must be false when creating a Todo item.");
+    }
+}
+
+interface IDateTimeProvider
+{
+    DateTime GetCurrentDateTime();
+}
+
+interface IDatabaseInitializer
+{
+    void Initialize();
+}
+
+class DateTimeProvider : IDateTimeProvider
+{
+    public DateTime GetCurrentDateTime() => DateTime.UtcNow;
+}
+
+class TodoDbInitializer : IDatabaseInitializer
+{
+    private readonly IServiceProvider _serviceProvider;
+
+    public TodoDbInitializer(IServiceProvider serviceProvider)
+    {
+        _serviceProvider = serviceProvider;
+    }
+
+    public void Initialize()
+    {
+        using (IServiceScope scope = _serviceProvider.CreateScope())
+        {
+            TodoDb dbContext = scope.ServiceProvider.GetRequiredService<TodoDb>();
+
+            // Check if the database has been created
+            if (dbContext.Database.EnsureCreated())
+            {
+                IDateTimeProvider? dateTimeProvider = scope.ServiceProvider.GetService<IDateTimeProvider>();
+
+                SeedData(dbContext, dateTimeProvider);
+            }
+        }
+    }
+
+    private void SeedData(TodoDb db, IDateTimeProvider? dateTimeProvider)
+    {
+        DateTime now = dateTimeProvider?.GetCurrentDateTime() ?? DateTime.UtcNow;
+
+        // Perform data seeding logic here
+        List<User> users = new List<User>
+        {
+            new User { Id = Guid.Parse("00000000-0000-0000-0000-000000000000"), FullName = "John Doe", Email = "john@example.com" },
+            new User { Id = Guid.Parse("00000000-0000-0000-0000-000000000001"), FullName = "Jane Smith", Email = "jane@example.com" },
+            new User { Id = Guid.Parse("00000000-0000-0000-0000-000000000002"), FullName = "Alice Johnson", Email = "alice@example.com" },
+            new User { Id = Guid.Parse("00000000-0000-0000-0000-000000000003"), FullName = "Bob Wilson", Email = "bob@example.com" },
+            new User { Id = Guid.Parse("00000000-0000-0000-0000-000000000004"), FullName = "Eva Davis", Email = "eva@example.com" },
+            new User { Id = Guid.Parse("00000000-0000-0000-0000-000000000005"), FullName = "Michael Thompson", Email = "michael@example.com" },
+            new User { Id = Guid.Parse("00000000-0000-0000-0000-000000000006"), FullName = "Sophia White", Email = "sophia@example.com" },
+            new User { Id = Guid.Parse("00000000-0000-0000-0000-000000000007"), FullName = "William Anderson", Email = "william@example.com" },
+            new User { Id = Guid.Parse("00000000-0000-0000-0000-000000000008"), FullName = "Olivia Davis", Email = "olivia@example.com" },
+            new User { Id = Guid.Parse("00000000-0000-0000-0000-000000000009"), FullName = "James Wilson", Email = "james@example.com" },
+            new User { Id = Guid.Parse("00000000-0000-0000-0000-000000000010"), FullName = "Emily Taylor", Email = "emily@example.com" }
+        };
+
+        Guid johnDoe = Guid.Parse("00000000-0000-0000-0000-000000000000");
+        Guid janeSmith = Guid.Parse("00000000-0000-0000-0000-000000000001");
+
+        List<Todo> todos = new List<Todo>
+        {
+            new Todo { Id = Guid.Parse("10000000-0000-0000-0000-000000000000"), Name = "Buy groceries", IsCompleted = false, CreatedAt = now, UserId = johnDoe},
+            new Todo { Id = Guid.Parse("10000000-0000-0000-0000-000000000001"), Name = "Finish project report", IsCompleted = true, UserId = janeSmith },
+            new Todo { Id = Guid.Parse("10000000-0000-0000-0000-000000000002"), Name = "Book doctor appointment", IsCompleted = false, UserId = janeSmith },
+            new Todo { Id = Guid.Parse("10000000-0000-0000-0000-000000000003"), Name = "Walk the dog", IsCompleted = true, UserId = johnDoe },
+            new Todo { Id = Guid.Parse("10000000-0000-0000-0000-000000000004"), Name = "Prepare for meeting", IsCompleted = false, UserId = johnDoe },
+            new Todo { Id = Guid.Parse("10000000-0000-0000-0000-000000000005"), Name = "Pick up kids from school", IsCompleted = true, UserId = janeSmith },
+            new Todo { Id = Guid.Parse("10000000-0000-0000-0000-000000000006"), Name = "Call the bank", IsCompleted = false, UserId = johnDoe },
+            new Todo { Id = Guid.Parse("10000000-0000-0000-0000-000000000007"), Name = "Study for the exam", IsCompleted = true, UserId = johnDoe },
+            new Todo { Id = Guid.Parse("10000000-0000-0000-0000-000000000008"), Name = "Clean the house", IsCompleted = false, UserId = janeSmith },
+            new Todo { Id = Guid.Parse("10000000-0000-0000-0000-000000000009"), Name = "Pay the bills", IsCompleted = true, UserId = janeSmith }
+        };
+
+        db.Users.AddRange(users);
+        db.Todos.AddRange(todos);
+        db.SaveChanges();
+    }
 }
